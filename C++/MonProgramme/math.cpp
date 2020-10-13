12c05011e5404b433ae1adc7de920935889dda05
Dyzee <dylan.poinsu@gmail.com>
2020-10-13 15:50:19 +0200

    remove c & c++ folders

--- C++/MonProgramme/math.cpp
+++
@@ -1,8 +0,0 @@
-#include "math.h"
-
-int ajouteDeux(int nombreRecu)
-{
-    int valeur(nombreRecu + 2);
-
-    return valeur;
-}
--- C++/MonProgramme/seconds.h
+++
@@ -1,6 +0,0 @@
-#ifndef SECONDS_H_INCLUDED
-#define SECONDS_H_INCLUDED
-
-int totalSeconds(int hour, int minute, int seconds);
-
-#endif // SECONDS_H_INCLUDED
